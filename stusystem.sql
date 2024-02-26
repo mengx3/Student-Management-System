a33c05afa44dd6c913647e8c660fbd10ba63c63e
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-02-25 21:51:08 -0500

    Update stusystem.sql
    Table structure for department

--- stusystem.sql
+++ stusystem.sql
@@ -37,44 +37,6 @@ INSERT INTO `course` VALUES ('18', 'gMath', '84', '5', '17');
 INSERT INTO `course` VALUES ('19', 'gMath', '81', '5', '12');
 INSERT INTO `course` VALUES ('20', 'gMath', '86', '5', '9');
 
--- ----------------------------
--- Table structure for department
--- ----------------------------
-DROP TABLE IF EXISTS `department`;
-CREATE TABLE `department` (
-  `did` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `director` varchar(255) DEFAULT NULL,
-  `capcity` int DEFAULT NULL,
-  `num` int DEFAULT '461',
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of department
--- ----------------------------
-INSERT INTO `department` VALUES ('J1009', 'Architecture Department', 'Xiaojuan', '500', '461');
-INSERT INTO `department` VALUES ('Y1018', 'Art Department', 'Xiaojun', '500', '461');
-INSERT INTO `department` VALUES ('S1014', 'Biology Department', 'Xiaomin', '500', '461');
-INSERT INTO `department` VALUES ('H1003', 'Chemistry Department', 'Xiaofang', '500', '461');
-INSERT INTO `department` VALUES ('C1001', 'Communications Department', 'Xiaohong', '500', '461');
-INSERT INTO `department` VALUES ('J1019', 'Computer Science Department', 'Xiaofang', '500', '461');
-INSERT INTO `department` VALUES ('J1017', 'Economics Department', 'Xiaohong', '500', '461');
-INSERT INTO `department` VALUES ('J1011', 'Education Department', 'Xiaobin', '500', '461');
-INSERT INTO `department` VALUES ('D1015', 'Electrical Engineering Department', 'Xiaotao', '500', '461');
-INSERT INTO `department` VALUES ('G1008', 'Engineering Department', 'Xiaoliang', '500', '461');
-INSERT INTO `department` VALUES ('W1007', 'Foreign Languages Department', 'Xiaohua', '500', '461');
-INSERT INTO `department` VALUES ('D1006', 'Geography Department', 'Xiaomei', '500', '461');
-INSERT INTO `department` VALUES ('L1005', 'History Department', 'Xiaoqiang', '500', '461');
-INSERT INTO `department` VALUES ('G1016', 'Management Department', 'Xiaoyan', '500', '461');
-INSERT INTO `department` VALUES ('S1012', 'Mathematics Department', 'Xiaolei', '500', '461');
-INSERT INTO `department` VALUES ('Y1004', 'Medical Department', 'Xiaolan', '500', '461');
-INSERT INTO `department` VALUES ('Y1020', 'Music Department', 'Xiaolan', '500', '461');
-INSERT INTO `department` VALUES ('W1013', 'Physics Department', 'Xiaona', '500', '461');
-INSERT INTO `department` VALUES ('Z1010', 'Politics and Law Department', 'Xiaohui', '500', '461');
-INSERT INTO `department` VALUES ('T1002', 'Sports Department', 'Xiaojun', '500', '461');
-
-
 
 -- ----------------------------
 -- Table structure for department
@@ -114,6 +76,22 @@ INSERT INTO `department` VALUES ('Z1010', 'Politics and Law Department', 'Xiaohu
 INSERT INTO `department` VALUES ('T1002', 'Sports Department', 'Xiaojun', '500', '461');
 
 
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
+  `did` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `director` varchar(255) DEFAULT NULL,
+  `capcity` int DEFAULT NULL,
+  `num` int DEFAULT '461',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of department
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for teacher
