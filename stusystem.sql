a5b67d99142d3a3b9dc4c6340e540fc18ab948af
qweszxc253 <149358487+qweszxc253@users.noreply.github.com>
2024-02-25 22:24:10 -0500

    Update stusystem.sql

--- stusystem.sql
+++ stusystem.sql
@@ -130,20 +130,58 @@ CREATE TABLE `teacher` (
 -- ----------------------------
 -- Records of teacher
 -- ----------------------------
-INSERT INTO `teacher` VALUES ('1', 'Qiao Feng', 'male', '35', 'Headmaster', '20000');
-INSERT INTO `teacher` VALUES ('2', 'Duan Yu', 'male', '28', 'Hall Master', '15000');
-INSERT INTO `teacher` VALUES ('3', 'Xu Zhu', 'male', '30', 'Protector', '18000');
-INSERT INTO `teacher` VALUES ('4', 'Murong Fu', 'male', '38', 'Hall Master', '22000');
-INSERT INTO `teacher` VALUES ('5', 'Wang Yuyan', 'female', '26', 'Hall Master\'s Wife', '16000');
-INSERT INTO `teacher` VALUES ('6', 'A Zhu', 'female', '32', 'Hall Master\'s Wife', '14000');
-INSERT INTO `teacher` VALUES ('7', 'A Bi', 'female', '24', 'Hall Master\'s Maid', '8000');
-INSERT INTO `teacher` VALUES ('8', 'Zhong Ling', 'female', '18', 'Hall Master\'s Maid', '6000');
-INSERT INTO `teacher` VALUES ('9', 'Mu Wanqing', 'female', '20', 'Hall Master\'s Maid', '7000');
-INSERT INTO `teacher` VALUES ('10', 'Meng Gu', 'female', '40', 'Abbess', '25000');
-INSERT INTO `teacher` VALUES ('11', 'Tianshan Tonglao', 'female', '99', 'Head Abbess', '30000');
-INSERT INTO `teacher` VALUES ('12', 'Li QiuShui', 'female', '88', 'Supreme Head', '28000');
-INSERT INTO `teacher` VALUES ('13', 'Wu Ya zi', 'male', '100', 'Ancestor', '35000');
-INSERT INTO `teacher` VALUES ('14', 'Ding Chunqiu', 'male', '66', 'Uncle', '26000');
-INSERT INTO `teacher` VALUES ('15', 'Su Xinghe', 'male', '72', 'Uncle', '24000');
-INSERT INTO `teacher` VALUES ('16', 'Duan Yanqing', 'male', '54', 'Protector', '12000');
-INSERT INTO `teacher` VALUES ('17', 'You Tanzhi', 'male', '20', 'Servant', '5000');
+INSERT INTO `teacher` VALUES ('1', 'Qiao Feng', 'male', '35', 'Principal', '20000');
+INSERT INTO `teacher` VALUES ('2', 'Duan Yu', 'male', '28', 'Department Chair', '15000');
+INSERT INTO `teacher` VALUES ('3', 'Xu Zhu', 'male', '30', 'Professor', '18000');
+INSERT INTO `teacher` VALUES ('4', 'Murong Fu', 'male', '38', 'Department Chair', '22000');
+INSERT INTO `teacher` VALUES ('5', 'Wang Yuyan', 'female', '26', 'Department Chairs Spouse', '16000');
+INSERT INTO `teacher` VALUES ('6', 'A Zhu', 'female', '32', 'Department Chairs Spouse', '14000');
+INSERT INTO `teacher` VALUES ('7', 'A Bi', 'female', '24', 'Faculty Assistant', '8000');
+INSERT INTO `teacher` VALUES ('8', 'Zhong Ling', 'female', '18', 'Faculty Assistant', '6000');
+INSERT INTO `teacher` VALUES ('9', 'Mu Wanqing', 'female', '20', 'Faculty Assistant', '7000');
+INSERT INTO `teacher` VALUES ('10', 'Meng Gu', 'female', '40', 'Dean', '25000');
+INSERT INTO `teacher` VALUES ('11', 'Tianshan Tonglao', 'female', '99', 'Head Dean', '30000');
+INSERT INTO `teacher` VALUES ('12', 'Li QiuShui', 'female', '88', 'Chancellor', '28000');
+INSERT INTO `teacher` VALUES ('13', 'Wu Ya zi', 'male', '100', 'Founder', '35000');
+INSERT INTO `teacher` VALUES ('14', 'Ding Chunqiu', 'male', '66', 'Senior Lecturer', '26000');
+INSERT INTO `teacher` VALUES ('15', 'Su Xinghe', 'male', '72', 'Senior Lecturer', '24000');
+INSERT INTO `teacher` VALUES ('16', 'Duan Yanqing', 'male', '54', 'Professor', '12000');
+INSERT INTO `teacher` VALUES ('17', 'You Tanzhi', 'male', '20', 'Research Assistant', '5000');
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sname` varchar(255) NOT NULL,
+  `sex` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `age` int NOT NULL,
+  `native` varchar(255) DEFAULT NULL COMMENT 'birthplace',
+  `dept` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES ('1', 'zhangsan', 'mail', '18', 'guangzhou', 'computer');
+INSERT INTO `student` VALUES ('2', 'Li Si', 'female', '19', 'Shenzhen, Guangdong', 'Mathematics Department');
+INSERT INTO `student` VALUES ('3', 'Wang Wu', 'male', '20', 'Zhuhai, Guangdong', 'Physics Department');
+INSERT INTO `student` VALUES ('4', 'Zhao Liu', 'female', '17', 'Shantou, Guangdong', 'Chemistry Department');
+INSERT INTO `student` VALUES ('5', 'Qian Qi', 'male', '18', 'Shaoguan, Guangdong', 'Biology Department');
+INSERT INTO `student` VALUES ('6', 'Sun Ba', 'female', '19', 'Foshan, Guangdong', 'Geography Department');
+INSERT INTO `student` VALUES ('7', 'Zhou Jiu', 'male', '20', 'Jiangmen, Guangdong', 'History Department');
+INSERT INTO `student` VALUES ('8', 'Wu Shi', 'female', '17', 'Zhanjiang, Guangdong', 'Art Department');
+INSERT INTO `student` VALUES ('9', 'Chen Yi', 'male', '18', 'Maoming, Guangdong', 'Sports Department');
+INSERT INTO `student` VALUES ('10', 'Zhang San', 'female', '19', 'Zhaoqing, Guangdong', 'Foreign Languages Department');
+INSERT INTO `student` VALUES ('11', 'Li Si', 'male', '20', 'Huizhou, Guangdong', 'Music Department');
+INSERT INTO `student` VALUES ('12', 'Wang Wu', 'female', '17', 'Meizhou, Guangdong', 'Education Department');
+INSERT INTO `student` VALUES ('13', 'Zhao Liu', 'male', '18', 'Shanwei, Guangdong', 'Politics and Law Department');
+INSERT INTO `student` VALUES ('14', 'Qian Qi', 'female', '19', 'Heyuan, Guangdong', 'Communications Department');
+INSERT INTO `student` VALUES ('15', 'Sun Ba', 'male', '20', 'Yangjiang, Guangdong', 'Medical Department');
+INSERT INTO `student` VALUES ('16', 'Zhou Jiu', 'female', '17', 'Qingyuan, Guangdong', 'Management Department');
+INSERT INTO `student` VALUES ('17', 'Wu Shi', 'male', '18', 'Dongguan, Guangdong', 'Economics Department');
+INSERT INTO `student` VALUES ('18', 'Chen Yi', 'male', '19', 'Zhongshan, Guangdong', 'Engineering Department');
+INSERT INTO `student` VALUES ('19', 'Li Si', 'male', '20', 'Chaozhou, Guangdong', 'Architecture Department');
+INSERT INTO `student` VALUES ('20', 'Wang Wu', 'male', '17', 'Jieyang, Guangdong', 'Electrical Engineering Department');
