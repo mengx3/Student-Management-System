751b3b93879fce96c06c2a25f440c4432527f34f
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-03-02 23:07:35 -0800

    Create Teacher.h

---
+++ Teacher.h
@@ -0,0 +1,22 @@
+#include <string>
+#include<iostream>
+using namespace std;
+
+#ifndef STUSYSTEMGUI_TEACHER_H
+#define STUSYSTEMGUI_TEACHER_H
+
+
+class Teacher {
+    int tid;
+
+    string tname;
+
+    string sex;
+
+    int age;
+
+    string title;
+
+    int salary;
+
+    int coid;
