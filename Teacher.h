f4a4b68ae0fa9e8e6556e7d8c742d27ff8ba9b84
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-03-02 23:07:58 -0800

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -20,3 +20,48 @@ class Teacher {
     int salary;
 
     int coid;
+
+public:
+    Teacher(const string& tname, const string& sex, int age, const string& title, int salary, int coid);
+
+    Teacher();
+
+    int getTid() const;
+
+    void setTid(int tid);
+
+    const string& getTname() const;
+
+    void setTname(const string& tname);
+
+    const string& getSex() const;
+
+    void setSex(const string& sex);
+
+    int getAge() const;
+
+    void setAge(int age);
+
+    const string& getTitle() const;
+
+    void setTitle(const string& title);
+
+    int getSalary() const;
+
+    void setSalary(int salary);
+
+    int getCoid() const;
+
+    void setCoid(int coid);
+
+    friend ostream& operator<<(ostream& os, const Teacher& teacher);
+
+    bool operator==(const Teacher& rhs) const;
+
+    bool operator!=(const Teacher& rhs) const;
+
+    virtual ~Teacher();
+};
+
+
+#endif //STUSYSTEMGUI_TEACHER_H
