369387cd94b51ce96a840a57f1594d6d2a2c02b3
JuanerW <112585205+JuanerW@users.noreply.github.com>
2024-03-18 21:20:42 -0400

    Update teststudentdao.cpp
    Added the test code for teststudentdao.cpp
    
    basically loop through the student list to check if the student is good.

--- teststudentdao.cpp
+++ teststudentdao.cpp
@@ -9,7 +9,18 @@
 
 
 int main() {
+//declare a studentdao
+    StudentDao studentDao;
 
+    //put the result into list
+    vector<Student>slist = studentDao.GetAllStudent();
+
+    //check every element in the list,and print
+    for (Student student : slist) {
+        //because studnet overwrte the "<<" operator
+        //so you can use cout<<student here
+        cout << student << endl;
+    }
 
     return 0;
 }
