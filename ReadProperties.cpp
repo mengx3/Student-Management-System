08625c0cc3ec0669600be34de6a747f2ddbcc178
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-03-18 22:14:26 -0400

    Update ReadProperties.cpp

--- ReadProperties.cpp
+++ ReadProperties.cpp
@@ -12,3 +12,22 @@ void ReadProperties::InitProperties(char* host, char* user, char* password, char
 
     //declare and open the file
     fstream file(R"(D:\C++file\StuSystemGui\com\properties.txt)", ios::in);
+    
+    //check if the file is open successfully
+    if (file.is_open()) {
+        //represent a line in file "properties.txt"
+        string line;
+        //using getline() function to read every line
+        while (getline(file, line)) {
+            //because the format in file like key=value,so we need to split them like dictionary
+            string key, value;
+
+            //get the position of '='
+            int pos = line.find('=');
+            //            cout<<"pos=="<<endl;
+
+                        //the ket here is a temporary variable
+                        //it represent the value in the left next to "="
+            key = line.substr(0, pos);
+            //the '=' should be ignored
+            value = line.substr(pos + 1);
