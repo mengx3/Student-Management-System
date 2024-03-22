0513ede7e828eff6b59242e5736ed294a3a0e84b
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-03-22 17:04:56 -0400

    Update StudentDao.cpp Supplementary vector<Student> StudentDao::CheckByName

--- StudentDao.cpp
+++ StudentDao.cpp
@@ -157,5 +157,28 @@ vector<Student> StudentDao::CheckByName(char* name) {
     sprintf(sql, "select * from student where sname like '%%%s%%'", name);
     printf("\nƒ£∫˝≤È—Øµƒsql:%s\n", sql);
 
+    //execute query
+    if (mysql_query(con, sql)) {
+        fprintf(stderr, "Failed to query data :Error %s", mysql_error(con));
+        printf("≤È—Ø ß∞‹£°\n");
+        return vector<Student>();
+    }
+
+    MYSQL_RES* res = mysql_store_result(con);
+
+    MYSQL_ROW row;
+    vector<Student>stuList;
+    while ((row = mysql_fetch_row(res))) {
+        Student stu;
+        stu.setId(atoi(row[0]));
+        stu.setSname(row[1]);
+        stu.setSex(row[2]);
+        stu.setAge(atoi(row[3]));
+        stu.setNative(row[4]);
+        stu.setDept(row[5]);
+        stuList.push_back(stu);
+        //        cout<<"here!"<<endl;
+    }
+
     return stuList;
 }
