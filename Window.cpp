105b6c344f85f059e521bed25c9e432591df2004
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-03-22 22:47:46 -0400

    Update Window.cpp

--- Window.cpp
+++ Window.cpp
@@ -17,3 +17,28 @@ int Window::exec()
 {
 	return getchar();
 }
+
+void Window::setTitle(const string& title)
+{
+
+	//set window's title
+	::SetWindowText(handle, title.c_str());
+
+
+}
+
+int Window::width()
+{
+	return ::getwidth();
+}
+
+int Window::height()
+{
+	return ::getheight();
+}
+
+void Window::clear()
+{
+
+	::cleardevice();
+}
