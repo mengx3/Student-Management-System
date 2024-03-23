21a82aed846b47af3ded5ec98199667e705e8206
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-03-22 22:45:43 -0400

    Update Window.h

--- Window.h
+++ Window.h
@@ -16,3 +16,32 @@ public:
 	//set window's title
 	void setTitle(const string& titile);
 
+	//get window's width
+	static int width();
+
+	//get window's height
+	static int height();
+
+	//to clear window
+	static void clear();
+
+	static void beginDraw();
+
+	static void flushDraw();
+
+	static void endDraw();
+
+	//the operation of keyboard or mouse to
+	//check if there is message
+	inline static bool hasMessage() { return ::peekmessage(&message, EX_MOUSE | EX_KEY); }
+	inline static const ExMessage& getMessage() { return message; }
+
+private:
+
+	//
+	HWND handle;
+
+	//Message
+	static ExMessage message;	
+
+};
