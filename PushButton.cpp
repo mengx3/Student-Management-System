7955e801fe24bc574e2f3065cbdefa1db23904e2
JuanerW <112585205+JuanerW@users.noreply.github.com>
2024-03-22 22:41:48 -0400

    Create PushButton.cpp
    PushButton class first created.
    updating functions later

---
+++ PushButton.cpp
@@ -0,0 +1,8 @@
+//
+// Created by bingfenhaochi on 2024/3/13.
+//
+
+#include "PushButton.h"
+
+PushButton::PushButton(const string& text, int mX, int mY, int mW, int mH) : BasicWidget(mX, mY, mW, mH), text(text) {}
+
