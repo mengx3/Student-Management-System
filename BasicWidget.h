c61d0201744a8a3ccb043bfeee488b6df974bd2c
qweszxc253 <149358487+qweszxc253@users.noreply.github.com>
2024-03-23 00:01:44 -0400

    Create BasicWidget.h

---
+++ BasicWidget.h
@@ -0,0 +1,36 @@
+#ifndef STUSYSTEMGUI_BASICWIDGET_H
+#define STUSYSTEMGUI_BASICWIDGET_H
+
+/**
+ * abstract class,is not allowed to create an instance
+ */
+
+class BasicWidget {
+
+public:
+    //constructor
+    BasicWidget(int mX, int mY, int mW, int mH);
+
+    int getMX() const;
+
+    int getMY() const;
+
+    int getMW() const;
+
+    int getMH() const;
+
+    void setFixSize(int wid, int heig);
+
+    void move(int x, int y);
+
+    virtual void show() = 0;
+
+protected:
+    int m_x;//
+    int m_y;
+    int m_w;
+    int m_h;
+};
+
+
+#endif //STUSYSTEMGUI_BASICWIDGET_H
