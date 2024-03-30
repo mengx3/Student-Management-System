4672f68b5d1a06ee98f769337e8de0b3d0350d4b
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-03-29 20:43:05 -0400

    Update PushButton.cpp

--- PushButton.cpp
+++ PushButton.cpp
@@ -1,7 +1,3 @@
-//
-// Created by bingfenhaochi on 2024/3/13.
-//
-
 #include "PushButton.h"
 
 PushButton::PushButton(const string& text, int mX, int mY, int mW, int mH) : BasicWidget(mX, mY, mW, mH), text(text) {}
