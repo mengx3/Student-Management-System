8277dc8376e34c88ece429c222960539211803d2
JuanerW <wangzhiyuanstan@outlook.com>
2024-04-05 16:27:34 -0400

    Update BasicWidget.h
    updated 3 functions' prototype
    and also an abstract class.

--- BasicWidget.h
+++ BasicWidget.h
@@ -5,6 +5,7 @@
  * abstract class,is not allowed to create an instance
  */
 
+#include<easyx.h>
 class BasicWidget {
 
 public:
@@ -26,13 +27,42 @@ public:
 
     void move(int x, int y);
 
+
+    //check if the mouse is stopping the Button
+    bool isOn();
+
+    //check if the button is clicked
+    bool isClicked(); 
+
+
     virtual void show() = 0;
+
+
+    //
+    virtual void event();
+
+    //setBackGroundColor
+    void setBackColor(COLORREF color);
+
+    //set color when mouse hovering in the button
+    void setMouseHoverColor(COLORREF color);
 
 protected:
     int m_x;//
     int m_y;
     int m_w;
     int m_h;
+
+
+    //currrnt button color
+    COLORREF currentColor = RGB(232, 232, 236);
+
+    //normal state color
+    COLORREF commonColor = RGB(232, 232, 236);
+
+    //when a mouse was hovering    
+    COLORREF mouseHoverColor = RED;
+
 };
 
 
