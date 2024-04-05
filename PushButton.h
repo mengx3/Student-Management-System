57f4dbc875948c4e5feb792f8143c25a0d275870
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-04-05 16:34:29 -0400

    Update PushButton.h

--- PushButton.h
+++ PushButton.h
@@ -12,3 +12,26 @@ private:
     // Message
     ExMessage M_Msg;
 
+
+public:
+    PushButton(const string& text = "BUTTON", int mX = 0, int mY = 0, int mW = 100, int mH = 30);
+
+    //rewrite show method
+    void show() override;
+
+    //getText
+    string getText();
+
+    //check if the mouse is stopping the Button
+    bool isOn();
+
+    //check if the button is clicked
+    bool isClicked();
+
+    //the circle of event
+    void eventLoop(const ExMessage& msg);
+
+};
+
+
+#endif //STUSYSTEMGUI_PUSHBUTTON_H
