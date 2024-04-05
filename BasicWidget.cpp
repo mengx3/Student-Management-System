b68bde2aac852c9160b1ec3ec27e43b0920beb47
JuanerW <wangzhiyuanstan@outlook.com>
2024-04-05 16:30:11 -0400

    Update BasicWidget.cpp
    code for the implementation of the new functions in the class of BasiccWidget.

--- BasicWidget.cpp
+++ BasicWidget.cpp
@@ -1,4 +1,5 @@
 #include "BasicWidget.h"
+#include"Window.h"
 
 BasicWidget::BasicWidget(int mX, int mY, int mW, int mH) : m_x(mX), m_y(mY), m_w(mW), m_h(mH) {}
 
@@ -29,4 +30,51 @@ void BasicWidget::move(int x, int y) {
     this->m_x = x;
     this->m_y = y;
 
+}
+
+bool BasicWidget::isOn()
+{
+
+    auto& M_Msg = Window::getMessage();
+
+    if (M_Msg.x >= m_x && M_Msg.x <= m_x + m_w && M_Msg.y >= m_y && M_Msg.y <= m_y + m_h) {
+        return true;
+    }
+    return false;
+}
+
+bool BasicWidget::isClicked()
+{
+
+    auto& M_Msg = Window::getMessage();
+
+    //check if is on
+    if (isOn()) {
+        //if is clicked
+        if (M_Msg.message == WM_LBUTTONDOWN) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void BasicWidget::event()
+{
+    //if mouse is not in button,currentColor = normalColor;
+    if (!isOn()) {
+        currentColor = commonColor;
+    }
+    else {
+        currentColor = mouseHoverColor;
+    }
+}
+
+void BasicWidget::setBackColor(COLORREF color)
+{
+    commonColor = color;
+}
+
+void BasicWidget::setMouseHoverColor(COLORREF color)
+{
+    mouseHoverColor = color;
 }
