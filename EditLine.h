cb873cf70a4119dd86786f40b44d4bb91c64abca
JuanerW <wangzhiyuanstan@outlook.com>
2024-04-05 16:40:54 -0400

    Update EditLine.h
    more implementation of EditLine file

--- EditLine.h
+++ EditLine.h
@@ -11,6 +11,23 @@ class EditLine :
 public:
     EditLine(int x = 0, int y = 0, int width = 200, int height = 40);
 
+
+    //rewrite show method
+    void show() override;
+
+    //set title
+    inline void setTitle(const string& t) { title = t; }
+
+    //set tip
+    inline void setTipMessage(const string& mes) { tipMessage = mes; }
+
+    //get the content
+    inline const string& getContent()const { return content; }
+
+    //clear the content
+    void clear() { content.clear(); }
+
+
 private:
 
     //the content
