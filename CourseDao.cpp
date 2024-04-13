d3428ecf6dac6eb6244ec2b60b4c51c2763ffb85
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-04-12 21:42:13 -0400

    Update CourseDao.cpp CourseDao::DelCourse

--- CourseDao.cpp
+++ CourseDao.cpp
@@ -31,3 +31,34 @@ int CourseDao::AddCourse(Course c)
 
     return 1;
 }
+
+int CourseDao::DelCourse(int cid)
+{
+    //declare a sql  sentence
+    char delSql[256];
+
+    ReadProperties::InitProperties(host, user, pw, dbName, port);
+
+    //    cout<<"come to here!"<<endl;
+
+    ConnectDB connectDb(host, user, pw, dbName, port);
+    connectDb.connect();
+
+    MYSQL* con = connectDb.getCon();
+
+    //    cout<<"come to here!"<<endl;
+
+        //using sprintf to init sql
+    sprintf(delSql, "delete from course where id = %d", cid);
+
+    //printf("\ndelSql\n:  %s", delSql);
+
+    //execute query
+    if (mysql_query(con, delSql)) {
+        fprintf(stderr, "Failed to delete data :Error %s", mysql_error(con));
+        printf("…æ≥˝ ß∞‹£°\n");
+        return 0;
+    }
+
+    return 1;
+}
