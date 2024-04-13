d5051d7a88bb9d7ca589c7c903d16035ea284dc3
qweszxc253 <149358487+qweszxc253@users.noreply.github.com>
2024-04-12 21:49:17 -0400

    Create TestTeacherDao.cpp

---
+++ TestTeacherDao.cpp
@@ -0,0 +1,34 @@
+#include"TeacherDao.h"
+
+
+//int main() {
+//	TeacherDao td;
+//	Teacher t1("DuanYu2", "male", 28,
+//		"Hall Master", 15000, 1);
+//	//td.AddTeacher(t1);
+//
+//	//test del
+//	//td.DelTeacher(28);
+//
+//	//test update
+//	//t1.setTname("zhangsanfengfeng");
+//	//t1.setTid(16);
+//	//td.UpdateTeacher(t1);
+//
+//	vector<Teacher>teaList;
+//
+//	//teaList = td.GetAllTeachers();
+//	//for (Teacher t : teaList) {
+//	//	cout << t << endl;
+//	//}
+//
+//	//char name[10];
+//	//strcpy(name, "ha");
+//
+//	//teaList = td.GetTeacherByTname(name);
+//	//for (Teacher t1 : teaList) {
+//	//	cout << t1 << endl;
+//	//}
+//
+//	return 0;
+//}
