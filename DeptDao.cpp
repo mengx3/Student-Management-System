0619d7728c83b0954bf3e2f1b9a6e7f020e8e57c
JuanerW <112585205+JuanerW@users.noreply.github.com>
2024-04-12 21:48:25 -0400

    Update DeptDao.cpp
    2 more implementation of the function

--- DeptDao.cpp
+++ DeptDao.cpp
@@ -62,3 +62,77 @@ int DeptDao::DelDept(string did)
     return 1;
 }
 
+int DeptDao::UpdateCourse(Deptartment dept)
+{
+    //declare a sql  sentence
+    char updateSql[256];
+
+    ReadProperties::InitProperties(host, user, pw, dbName, port);
+
+    //    cout<<"come to here!"<<endl;
+
+    ConnectDB connectDb(host, user, pw, dbName, port);
+    connectDb.connect();
+
+    MYSQL* con = connectDb.getCon();
+
+    //    cout<<"come to here!"<<endl;
+
+        //using sprintf to init sql
+    sprintf(updateSql, "update department set name='%s',director='%s',capcity=%d,num=%d where did='%s'",
+        dept.getName().c_str(), dept.getDirector().c_str(), dept.getCapcity(), dept.getNum(), dept.getDid().c_str());
+
+    //execute query
+    if (mysql_query(con, updateSql)) {
+        fprintf(stderr, "Failed to insert data :Error %s", mysql_error(con));
+        printf("fail！\n");
+        return 0;
+    }
+
+    return 1;
+}
+
+vector<Deptartment> DeptDao::GetAllDepts()
+{
+    vector<Deptartment>dList;
+
+    //declare a sql  sentence
+    char sql[256];
+
+    ReadProperties::InitProperties(host, user, pw, dbName, port);
+
+    //    cout<<"come to here!"<<endl;
+
+    ConnectDB connectDb(host, user, pw, dbName, port);
+    connectDb.connect();
+
+    MYSQL* con = connectDb.getCon();
+
+    //    cout<<"come to here!"<<endl;
+
+        //using sprintf to init sql
+    sprintf(sql, "select * from department");
+
+    //execute query
+    if (mysql_query(con, sql)) {
+        fprintf(stderr, "Failed to query data :Error %s", mysql_error(con));
+        printf("fail！\n");
+        return vector<Deptartment>();
+    }
+
+    MYSQL_RES* res = mysql_store_result(con);
+
+    MYSQL_ROW row;
+    while ((row = mysql_fetch_row(res))) {
+
+        Deptartment dept;
+        dept.setDid(row[0]);
+        dept.setName(row[1]);
+        dept.setDirector(row[2]);
+        dept.setCapcity(atoi(row[3]));
+        dept.setNum(atoi(row[4]));
+        dList.push_back(dept);
+    }
+
+    return dList;
+}
