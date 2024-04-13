1f6981db85bbac3e32ef4d29a8049a7475d329b6
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-04-12 21:43:36 -0400

    Update CourseDao.cpp CourseDao::GetAllCourse

--- CourseDao.cpp
+++ CourseDao.cpp
@@ -93,3 +93,32 @@ int CourseDao::UpdateCourse(Course course)
 
     return 1;
 }
+
+vector<Course> CourseDao::GetAllCourse()
+{
+    vector<Course>cList;
+
+    //declare a sql  sentence
+    char sql[256];
+
+    ReadProperties::InitProperties(host, user, pw, dbName, port);
+
+    //    cout<<"come to here!"<<endl;
+
+    ConnectDB connectDb(host, user, pw, dbName, port);
+    connectDb.connect();
+
+    MYSQL* con = connectDb.getCon();
+
+    //    cout<<"come to here!"<<endl;
+
+        //using sprintf to init sql
+    sprintf(sql, "select * from course");
+
+    //execute query
+    if (mysql_query(con, sql)) {
+        fprintf(stderr, "Failed to query data :Error %s", mysql_error(con));
+        printf("≤È—Ø ß∞‹£°\n");
+        return vector<Course>();
+    }
+}
