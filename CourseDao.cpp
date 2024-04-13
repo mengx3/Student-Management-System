bac025552d81323783979fe1643f1de1713061ea
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-04-12 21:41:41 -0400

    Create CourseDao.cpp CourseDao::AddCourse

---
+++ CourseDao.cpp
@@ -0,0 +1,33 @@
+#include "CourseDao.h"
+
+int CourseDao::AddCourse(Course c)
+{
+
+
+    //declare a sql  sentence
+    char insertSql[256];
+
+    ReadProperties::InitProperties(host, user, pw, dbName, port);
+
+    //    cout<<"come to here!"<<endl;
+
+    ConnectDB connectDb(host, user, pw, dbName, port);
+    connectDb.connect();
+
+    MYSQL* con = connectDb.getCon();
+
+    //    cout<<"come to here!"<<endl;
+
+        //using sprintf to init sql
+    sprintf(insertSql, "insert into course (name,score,value) values ('%s',%d,%d)",
+        c.getName().c_str(), c.getScore(),c.getValue());
+
+    //execute query
+    if (mysql_query(con, insertSql)) {
+        fprintf(stderr, "Failed to insert data :Error %s", mysql_error(con));
+        printf("≤Â»Î ß∞‹£°\n");
+        return 0;
+    }
+
+    return 1;
+}
